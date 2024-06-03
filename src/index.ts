50a04c7257aff361b3ff84d33d25c043e9064280
jessanjos <jessicarocha.anjos@gmail.com>
2024-06-02 22:41:39 -0300

    Add log lib

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import router from './routes';
+import morganMiddleware from './config/morganMiddleware'
 
 const PORT = process.env.PORT || 4000; // Server port
 const HOSTNAME = process.env.HOSTNAME || 'http://localhost'; // Server host
 
 const app = express();
+app.use(morganMiddleware);
 app.use(express.json()); //Adding json encoding
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
