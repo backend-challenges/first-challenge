f33ac22f4ec4781ee568f7567d0cd3e574bc0d9c
jessanjos <jessicarocha.anjos@gmail.com>
2024-06-02 22:23:53 -0300

    Add json encoding

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 4000; // Server port
 const HOSTNAME = process.env.HOSTNAME || 'http://localhost'; // Server host
 
 const app = express();
+app.use(express.json()); //Adding json encoding
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: ['http://localhost:3000']
 }));
